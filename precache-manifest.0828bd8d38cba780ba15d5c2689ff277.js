64434927b2b68dc4f3bf0e691d7c252617980b3b
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-09-25 09:57:21 +0530

    Updates

---
+++ precache-manifest.0828bd8d38cba780ba15d5c2689ff277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f346c0eeed4bbf8b1fd46440fdfd1c4",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "5aeaafc36402f08e2af1",
+    "url": "/restaurant/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee5344c40a72371bde8d",
+    "url": "/restaurant/static/js/2.c96482db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant/static/js/2.c96482db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aeaafc36402f08e2af1",
+    "url": "/restaurant/static/js/main.b5431c8c.chunk.js"
+  },
+  {
+    "revision": "d8b34c76c7241bdb1bc6",
+    "url": "/restaurant/static/js/runtime-main.874cb75f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/restaurant/static/media/logo.5d5d9eef.svg"
+  }
+]);
