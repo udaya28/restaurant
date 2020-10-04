5ce8dc4c2c2989ec7ae9940666cd15c904857eb2
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-10-04 12:42:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-      </header>
+      <header className="App-header"/>
+      <h1>hello</h1>
+      
+     
     </div>
   );
 }
