62f33b1b9dddf3d5dde53c40d2f3db1c87bdc16a
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-10-07 20:29:09 +0530

    added bootstrap locally

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import 'bootstrap/dist/js/bootstrap.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
