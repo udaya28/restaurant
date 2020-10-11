33d3eb4696f64c88b092c56f34ab19c0975b454d
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-10-11 16:09:54 +0530

    added hover effect

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './card.styles.css';
 export const Card = ({ url, name }) => {
   return (
-    <div className="card1">
+    <div className="card1 shadow-sm">
       <img src={url} alt={name} />
       <p>{name}</p>
     </div>
